8a338f4cabdaaba98735ea13a87ba509551b7c36
Jesus Vasquez Medina <jesusvasquezm098@gmail.com>
2024-03-18 12:07:17 -0300

    script done

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idTagSong) {
+    const element = document.querySelector(idTagSong);
+    if(element != null && element.localName === 'audio') {
+        element.play();
+    }
+    else{
+        console.log('Elemento não encontrado ou seletor não válido');
+    }
 
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-while(contador < listaDeTeclas.length){
-    listaDeTeclas[contador].onclick = tocaSomPom;
-    contador= contador + 1;
+    const teclado = listaDeTeclas[contador];
+    const instrumento = teclado.classList[1];
 
-    console.log(contador);
+    const idAudio = `#som_${instrumento}`;
+
+    teclado.onclick = function () {
+        tocaSom(idAudio);
+    }
+    
+    teclado.onkeydown = function (evento) {
+
+        console.log(evento.code);
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            teclado.classList.add('ativa');
+        }
+        
+        
+    }
+
+    teclado.onkeydown = function () {
+        teclado.classList.remove('ativa');
+    }
 }
